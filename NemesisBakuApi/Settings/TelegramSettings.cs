572e4ff0678c4f68d5708f26e852099d7d365c5e
Ibadulla Huseynzade <Husey_iz52@itstep.edu.az>
2026-07-22 15:57:30 +0400

    edit telegram bot name

--- NemesisBakuApi/Settings/TelegramSettings.cs
+++ NemesisBakuApi/Settings/TelegramSettings.cs
@@ -7,7 +7,7 @@ public class TelegramSettings
     public bool Enabled { get; set; } = true;
     public string BotToken { get; set; } = string.Empty;
     public string WebhookSecret { get; set; } = string.Empty;
-    public string BotUsername { get; set; } = "nemesisbakuMonitorBot";
+    public string BotUsername { get; set; } = "nemesisbakuSifarisBot";
     public string PublicBaseUrl { get; set; } = "https://nemesisbaku.az";
     public string SiteBaseUrl { get; set; } = "https://nemesisbaku.az";
 }
